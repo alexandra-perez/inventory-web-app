2dafd9da67ac556c7cba06a91813eccb429d0ddd
alexandra-perez <alexandraperezny@gmail.com>
2024-03-20 19:58:28 -0400

    Add form event listeners

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # inventory-web-app
+Welcome to the Inventory Management App! This application is designed to help users efficiently track and manage their inventory of cleaning products. With a user-friendly interface and simple functionality, this app allows users to easily add new cleaning products to their inventory and view them in a populated table format.
+
+## Getting Started
+## Installation
+1. Clone the repository from GitHub.
+2. Navigate to the project directory.
+3. Start your web server.
+
+## Features
+### Adding New Products
+- Users can easily add a new cleaning product by filling out a simple form.
+- The form includes fields for product brand, name, and any other relevant information.
+- Upon submission, the product details are automatically added to the inventory table.
+
+### Viewing Inventory
+- The main dashboard of the app displays a table populated with all existing cleaning products in the inventory.
+- Users can view product names, categories, and other details at a glance.
+- The table provides easy sorting and filtering options for enhanced usability.
+
+## Credits
+Logo by logoipsum.com
+Icons by svgrepo.com
--- script.js
+++ script.js
@@ -1,52 +1,125 @@
 /*
 Functionality to add:
-  - Local Storage
-  - Data population for all columns
-  - Delete button deleting existing entries
-  - Warning modal for user on-click delete button
+  - Edit button can't be edited
+  - Edit qty updates total inventory
 */
 
-const form = document.querySelector('form');
+// Populate header
+const headerDate = document.querySelector('.header__date');
+headerDate.innerText = new Date();
+headerDate.style.color = '#c6cdd7';
 
+// Set line items count
+let lineItemsCount = 4;
+const totalLineItems = document.querySelector('.total-lines-count');
+totalLineItems.innerText = `Total line items: ${lineItemsCount}`;
+
+// Set inventory count
+let inventoryCount = 0;
+const totalInventory = document.querySelector('.total-inventory-count');
+
+const quantities = document.querySelectorAll('.qty');
+for (const quantity of quantities) {
+  inventoryCount += Number(quantity.innerText);
+}
+totalInventory.innerText = `Total items in inventory (${inventoryCount})`;
+
+// Add form logic
+const form = document.querySelector('form');
+let selectedRow = null;
+
+// Set current date as min exp. date allowed
 let today = new Date().toISOString().split('T')[0];
 document.getElementsByName('expirationDate')[0].setAttribute('min', today);
 
+// Add edit functionality
+const existingEditBtns = document.querySelectorAll('.edit-button');
+for (const existingEditBtn of existingEditBtns) {
+  existingEditBtn.addEventListener('click', (event) => {
+    selectedRow = event.target.parentNode.parentNode;
+    event.target.parentNode.parentNode.classList.toggle('highlight');
+    selectedRow.contentEditable = 'true';
+  });
+}
+
+// Show modal on user - click delete button.
+// Set selected row for deletion.
+const existingBtns = document.querySelectorAll('.delete-button');
+for (const existingBtn of existingBtns) {
+  existingBtn.addEventListener('click', (event) => {
+    showModal();
+    selectedRow = event.target.parentNode.parentNode;
+  });
+}
+
+// Add form submission event listener
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const productBrand =
     form.productBrand.value.charAt(0).toUpperCase() +
     form.productBrand.value.slice(1);
-  const productName =
-    form.productName.value.charAt(0).toUpperCase() +
-    form.productName.value.slice(1);
-
-  const productCost = form.productCost.value;
+  const productDescription =
+    form.productBrand.value +
+    ' ' +
+    form.productDescription.value.charAt(0).toUpperCase() +
+    form.productDescription.value.slice(1);
+  const productQty = form.productQty.value;
+  const productCost = Number(form.productCost.value).toFixed(2).toString();
   const expirationDate = form.expirationDate.value;
   const productCategory = form.productCategory.value;
   const stockStatus = form.stockStatus.value;
 
-  addProductToTable(productBrand, productName);
+  // Populate inventory table
+  addProductToTable(
+    productBrand,
+    productDescription,
+    productQty,
+    productCost,
+    expirationDate,
+    productCategory,
+    stockStatus
+  );
+  // Increment and display line items and inventory count
+  lineItemsCount++;
+  totalLineItems.innerText = `Total line items: ${lineItemsCount}`;
+
+  inventoryCount += Number(productQty);
+  totalInventory.innerText = `Total items in inventory (${inventoryCount})`;
 
   // form.reset();
 });
 
+// Populate inventory table
 function addProductToTable(
   productBrand,
-  productName,
+  productDescription,
+  productQty,
   productCost,
   expirationDate,
   productCategory,
   stockStatus
 ) {
-  const table = document.querySelector('.productTable');
+  const table = document.querySelector('.product-table');
   const newRow = document.createElement('tr');
+  newRow.classList.add('product-table__tr');
+
+  const editCell = document.createElement('td');
+  const editButton = document.createElement('button');
+  editButton.innerHTML = 'Edit';
+  editCell.classList.add('edit-col');
+  editButton.classList.add('edit-button');
+  editCell.appendChild(editButton);
 
   const productBrandCell = document.createElement('td');
   productBrandCell.innerText = productBrand;
 
-  const productNameCell = document.createElement('td');
-  productNameCell.innerText = productName;
+  const productDescriptionCell = document.createElement('td');
+  productDescriptionCell.innerText = productDescription;
+
+  const qtyCell = document.createElement('td');
+  qtyCell.innerText = productQty;
+  qtyCell.classList.add('qty');
 
   const productCostCell = document.createElement('td');
   productCostCell.innerText = productCost;
@@ -60,8 +133,10 @@ function addProductToTable(
   const stockStatusCell = document.createElement('td');
   stockStatusCell.innerText = stockStatus;
 
+  newRow.appendChild(editCell);
   newRow.appendChild(productBrandCell);
-  newRow.appendChild(productNameCell);
+  newRow.appendChild(productDescriptionCell);
+  newRow.appendChild(qtyCell);
   newRow.appendChild(productCostCell);
   newRow.appendChild(expirationDateCell);
   newRow.appendChild(productCategoryCell);
@@ -69,17 +144,57 @@ function addProductToTable(
 
   const deleteBtn = document.createElement('button');
   deleteBtn.innerHTML = 'Delete';
-  deleteBtn.classList.add("delete-button");
+  deleteBtn.classList.add('delete-button');
 
-  newRow.append(deleteBtn);
+  const td = document.createElement('td');
+  td.appendChild(deleteBtn);
+  newRow.append(td);
+  td.classList.add('delete-col');
 
-  deleteBtn.addEventListener('click', () => {
-    newRow.remove();
+  // Add edit functionality for new rows
+  editButton.addEventListener('click', (event) => {
+    selectedRow = event.target.parentNode.parentNode;
+    selectedRow.contentEditable = 'true';
+    event.target.parentNode.parentNode.classList.toggle('highlight');
+  });
+
+  // Add delete functionality for new rows
+  deleteBtn.addEventListener('click', (event) => {
+    showModal();
+    selectedRow = event.target.parentNode.parentNode;
   });
 
   table.appendChild(newRow);
 }
 
-const headerDate = document.querySelector('.header__date');
-headerDate.innerText = new Date();
-headerDate.style.color = '#c6cdd7';
+// Show/Hide warning modal
+function showModal() {
+  const warningModal = document.querySelector('.warning-modal');
+  warningModal.style.display = 'block';
+
+  const confirmBtn = document.querySelector('.confirm-btn');
+  const cancelBtn = document.querySelector('.cancel-btn');
+
+  cancelBtn.addEventListener('click', hideModal);
+  confirmBtn.addEventListener('click', confirmDelete);
+}
+
+function hideModal() {
+  const warningModal = document.querySelector('.warning-modal');
+  warningModal.style.display = 'none';
+}
+
+// Delete table row
+function confirmDelete() {
+  const quantityCell = selectedRow.querySelector('.qty');
+  console.log(quantityCell);
+  inventoryCount -= Number(quantityCell.textContent);
+  totalInventory.innerText = `Total items in inventory (${inventoryCount})`;
+
+  lineItemsCount--;
+  totalLineItems.innerText = `Total line items: ${lineItemsCount}`;
+
+  selectedRow.remove();
+  hideModal();
+  selectedRow = null;
+}
